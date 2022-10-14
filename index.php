3e0ea0953595a1b245c44c7c5213eb30e03fecff
MdKabirBilal <86850287+MdKabirBilal@users.noreply.github.com>
2022-10-14 08:52:12 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require 'init.php';
+?>
+<?php include 'head.php'; ?>
+<body>
+   <div class="container start-container">
+     <div class="col-md-6 title-text">
+     
+     	<h2>Graphic Era (Deemed to be) University DehraDun</h2>
+     	<h3>Mini Project Allocation Portal</h3>
+<br> <br>
+<h4> default id, password - admin, password </h4>
+     </div>
+     <div class="col-md-4">
+     	<?php include 'login_form.php'; ?>
+     </div>
+   </div>
+<?php include 'footer.php'; ?>
